225b889fe5d98064e8e4259d5176b3fe7f559c00
Jay T <torrcorp@gmail.com>
2018-06-20 18:48:13 -0600

    Chapter 6 Completed with build gradle error.

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,10 +1,15 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -48,13 +53,30 @@ public class CheatActivity extends AppCompatActivity {
           mAnswerTextView.setText(R.string.false_button);
         }
         setAnswerShownResult(true);
+
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+          int cx = mShowAnswerButton.getWidth() / 2;
+          int cy = mShowAnswerButton.getHeight() / 2;
+          float radius = mShowAnswerButton.getWidth();
+          Animator anim = ViewAnimationUtils
+              .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+          anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+              super.onAnimationEnd(animation);
+              mShowAnswerButton.setVisibility(View.INVISIBLE);
+            }
+          });
+          anim.start();
+        } else {
+          mShowAnswerButton.setVisibility(View.INVISIBLE);
+        }
       }
     });
-  }
 
-  private void setAnswerShownResult(boolean isAnswerShown) {
-    Intent data = new Intent();
-    data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
-    setResult(RESULT_OK, data);
+    private void setAnswerShownResult ( boolean isAnswerShown){
+      Intent data = new Intent();
+      data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+      setResult(RESULT_OK, data);
+    }
   }
-}
